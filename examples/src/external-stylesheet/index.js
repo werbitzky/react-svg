026a004c065720c307623ffc410ec48f8913abb3
Tane Morgan <464864+tanem@users.noreply.github.com>
2019-12-16 10:20:51 +1300

    Switch examples to TS

--- examples/src/external-stylesheet/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { ReactSVG } from 'react-svg'
-import './style.css'
-
-ReactDOM.render(
-  <ReactSVG className="wrapper" src="svg.svg" />,
-  document.getElementById('root')
-)
